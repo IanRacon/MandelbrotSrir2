3d12337fa64b15a80489d7f576825efb47c0997a
IanRacon <rafalcosz@gmail.com>
2017-06-09 23:16:57 +0200

    Small fixes

--- Client.java
+++ Client.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.io.PrintWriter;
 import java.io.*;
 
+
 public class Client
 {
 	public static void main( String arg[] ) throws Exception
@@ -49,12 +50,12 @@ public class Client
 			chunkParams1.x1 = x1;
 			chunkParams1.y1 = y1;
 			chunkParams1.x2 = x2;
-			chunkParams1.y2 = y2;
+			chunkParams1.y2 = (y1+y2)/2;
 			chunkParams1.step = step;
 
 			ChunkCoords chunkParams2 = new ChunkCoords();
-			chunkParams2.x1 = (x2-x1)/2;
-			chunkParams2.y1 = y1;
+			chunkParams2.x1 = x1;
+			chunkParams2.y1 = (y1+y2)/2 + step;
 			chunkParams2.x2 = x2;
 			chunkParams2.y2 = y2;
 			chunkParams2.step = step;
@@ -68,15 +69,18 @@ public class Client
 			result1 = remoteObj1.mandelbrot(chunkParams1);
 			result2 = remoteObj2.mandelbrot(chunkParams2);
 
+			System.out.println("Data from server 1: rows: " + result1.mandelbrotValues.length + ", cols: " + result1.mandelbrotValues[0].length);
+			System.out.println("Data from server 2: rows: " + result2.mandelbrotValues.length + ", cols: " + result2.mandelbrotValues[0].length);
+
 			int[][] result = append(result1.mandelbrotValues, result2.mandelbrotValues);
 			/* print results from returned parameters object */
 			/*** Step 4:
 			   * Print results obtained by RMI.
 			   */
 
-			System.out.println(Arrays.deepToString(result1.mandelbrotValues));
+			//System.out.println(Arrays.deepToString(result1.mandelbrotValues));
 			//System.out.println(Arrays.deepToString(result2.mandelbrotValues));
-			exportBoardToFile(result1, x1, x2, y1, y2, step, rows, cols); 
+			exportBoardToFile(result, x1, x2, y1, y2, step, rows, cols); 
 		}
 		catch( Exception e )
 		{
@@ -84,7 +88,7 @@ public class Client
 			return;
 		}
 	}
-	private static void exportBoardToFile(ChunkData result, double minx, 
+	private static void exportBoardToFile(int[][] result, double minx, 
 			double maxx, double miny, double maxy, double step,
 			int rows, int cols)
 	{
@@ -104,7 +108,7 @@ public class Client
 				for (int col = 0; col < cols; ++col) 
 				{
 					double x = x_mult * col + minx;
-					out.println( x + "\t" + y + "\t" + result.mandelbrotValues[row][col]);
+					out.println( x + "\t" + y + "\t" + result[row][col]);
 				}
 				out.println();
 			}
@@ -120,5 +124,5 @@ public class Client
 		System.arraycopy(a, 0, result, 0, a.length);
 		System.arraycopy(b, 0, result, a.length, b.length);
 		return result;
-    	}	
+    }	
 }

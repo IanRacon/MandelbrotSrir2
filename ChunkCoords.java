ec4e2d57c382585ae46ced7c6b9e1127b95783ca
IanRacon <rafalcosz@gmail.com>
2017-06-10 00:20:08 +0200

    Added multithreading

--- ChunkCoords.java
+++ ChunkCoords.java
@@ -2,6 +2,14 @@ import java.io.*;
 
 public class ChunkCoords implements Serializable
 {
+	ChunkCoords(double x1, double x2, double y1, double y2, double step)
+	{
+		this.x1 = x1;
+		this.x2 = x2;
+		this.y1 = y1;
+		this.y2 = y2;
+		this.step = step;
+	}
 	double x1;
 	double y1;
 	double x2;
--- Client.java
+++ Client.java
@@ -3,6 +3,36 @@ import java.util.Arrays;
 import java.io.PrintWriter;
 import java.io.*;
 
+class RemoteMandelbrot extends Thread
+{
+	private String threadName;
+	ChunkCoords chunkParams;
+	IMandelbrotResolver remoteMethod;
+	ChunkData chunkData;
+
+	RemoteMandelbrot(String name, ChunkCoords p_chunkParams, IMandelbrotResolver p_remoteMethod)
+	{
+		threadName = name;
+		chunkParams = p_chunkParams;
+		remoteMethod = p_remoteMethod;
+	}
+	public void run()
+	{
+		try
+		{
+			System.out.println("Starting remote method");
+			chunkData = remoteMethod.mandelbrot(chunkParams);
+			System.out.println("Remote method execution ends");
+		}catch(RemoteException e)
+		{
+			System.out.println("Thread: " + threadName + " interrupted by exception: " + e);
+		}
+	}
+	public ChunkData getResults()
+	{
+		return chunkData;
+	}
+}
 
 public class Client
 {
@@ -46,41 +76,38 @@ public class Client
 			/*** Step 2:
 			   * Prepare ChunkCoords "parameters".
 			   */
-			ChunkCoords chunkParams1 = new ChunkCoords();
-			chunkParams1.x1 = x1;
-			chunkParams1.y1 = y1;
-			chunkParams1.x2 = x2;
-			chunkParams1.y2 = (y1+y2)/2;
-			chunkParams1.step = step;
-
-			ChunkCoords chunkParams2 = new ChunkCoords();
-			chunkParams2.x1 = x1;
-			chunkParams2.y1 = (y1+y2)/2 + step;
-			chunkParams2.x2 = x2;
-			chunkParams2.y2 = y2;
-			chunkParams2.step = step;
-
+			ChunkCoords chunkParams1 = new ChunkCoords(x1, x2, y1, (y1+y2)/2, step);
+			ChunkCoords chunkParams2 = new ChunkCoords(x1, x2, (y1+y2)/2 + step, y2, step);
+			RemoteMandelbrot remote1 = new RemoteMandelbrot("Remote-1", chunkParams1, remoteObj1);
+			RemoteMandelbrot remote2 = new RemoteMandelbrot("Remote-2", chunkParams2, remoteObj2);
 			/* execute RMI call */
 			/*** Step 3:
 			   * Call remote method of obj object. 
 			   */
-			ChunkData result1;
-			ChunkData result2;
-			result1 = remoteObj1.mandelbrot(chunkParams1);
-			result2 = remoteObj2.mandelbrot(chunkParams2);
+			remote1.start();
+			remote2.start();
 
-			System.out.println("Data from server 1: rows: " + result1.mandelbrotValues.length + ", cols: " + result1.mandelbrotValues[0].length);
-			System.out.println("Data from server 2: rows: " + result2.mandelbrotValues.length + ", cols: " + result2.mandelbrotValues[0].length);
+			try
+			{
+				remote1.join();
+				remote2.join();
+			}catch(Exception e)
+			{
+				System.out.println(e);
+			}
 
-			int[][] result = append(result1.mandelbrotValues, result2.mandelbrotValues);
+			int[][] results1 = remote1.getResults().mandelbrotValues;
+			int[][] results2 = remote2.getResults().mandelbrotValues;
+			System.out.println("Data from server 1: rows: " + results1.length + ", cols: " + results1[0].length);
+			System.out.println("Data from server 2: rows: " + results2.length + ", cols: " + results2[0].length);
+
+			int[][] aggregatedResult = append(results1, results2);
+
 			/* print results from returned parameters object */
 			/*** Step 4:
 			   * Print results obtained by RMI.
 			   */
-
-			//System.out.println(Arrays.deepToString(result1.mandelbrotValues));
-			//System.out.println(Arrays.deepToString(result2.mandelbrotValues));
-			exportBoardToFile(result, x1, x2, y1, y2, step, rows, cols); 
+			exportBoardToFile(aggregatedResult, x1, x2, y1, y2, step, rows, cols); 
 		}
 		catch( Exception e )
 		{
